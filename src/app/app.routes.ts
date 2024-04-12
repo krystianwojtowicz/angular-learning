f57ed81389f7732f910b7dd25ed0997b1404d0da
Krystian <krystian1wojtowicz@gmail.com>
2024-04-12 12:43:00 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Home Page' },
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: { title: 'Contact Page' },
+  },
+];
+
+export default routes;
